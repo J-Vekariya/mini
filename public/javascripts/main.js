62e26b0dd446d49d8ec2793533a8e78ebdbad23b
Jayesh <jgv1593@gmail.com>
2017-02-12 18:57:35 +0530

    First Commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    const pre = Prelodr();
+    var next = 1;
+    $(".add-more").click(function(e){
+        e.preventDefault();
+        var addto = "#field" + next;
+        var addRemove = "#field" + (next);
+        next = next + 1;
+        var newIn = '<input autocomplete="off" class="input form-control text" id="field' + next + '" name="field' + next + '" type="text" placeholder="Link goes here">';
+        var newInput = $(newIn);
+        var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button></div><div id="field">';
+        var removeButton = $(removeBtn);
+        $(addto).after(newInput);
+        $(addRemove).after(removeButton);
+        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
+        $("#count").val(next);  
+        
+            $('.remove-me').click(function(e){
+                e.preventDefault();
+                var fieldNum = this.id.charAt(this.id.length-1);
+                var fieldID = "#field" + fieldNum;
+                $(this).remove();
+                $(fieldID).remove();
+            });
+    })
+
+    $("#minify-button").click(function(){
+        pre.show('Please wait it will take a while...');
+        var ele = document.getElementsByClassName('text');
+        var urls = [];
+        for(var i=0;i < ele.length;i++){
+            urls.push(ele[i].value);
+        }
+        var d = {
+            "links" : urls
+        }
+          var xhr = new XMLHttpRequest();
+          xhr.open("post", "http://localhost:3000/users/minify", true);
+          xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+          // send the collected data as JSON
+          xhr.send(JSON.stringify(d));
+
+          xhr.onloadend = function () {
+            // done
+           pre.hide();
+          };
+    })
+
+
+    
+});
---
+++ routes/users.js
@@ -0,0 +1,115 @@
+var express = require('express');
+var router = express.Router();
+var compressor = require('node-minify');
+var fs = require('fs');
+var request = require('request');
+var https = require('https');
+var http = require('http');
+var ip = require('ip');
+var path=require('path');
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+})
+
+router.post('/minify', function(req, res) {
+	var d = req.body;
+	var folder = 'data/'+new Date().getTime() + '/';
+	fs.mkdir(folder,function(e){
+	    if(!e || (e && e.code === 'EEXIST')){
+	        //do something with contents
+	    } else {
+	        //debug
+	        console.log(e);
+	    }
+	});
+	var download = function(url, dest, callback){
+
+	    request.get(url)
+	    .on('error', function(err) {console.log(err)} )
+	    .pipe(fs.createWriteStream(dest))
+	    .on('close', callback);
+
+	};
+	var urlList = d.links;
+	var files = [];
+	var i = 0;
+	function getFiles(callbackjs, callbackcss){
+		var extension;
+		urlList.forEach( function(str) {
+			var filename =  str.split('/').pop();
+			console.log('Downloading ' + filename);
+			extension = filename.split('.').pop();
+			files.push(folder + filename);
+			download(str, files[i], function(){
+				console.log('Finished Downloading' + filename);	
+			});
+			i++;
+		});
+		if(extension == 'js'){
+			callbackjs();
+		}
+		else if(extension == 'css'){
+			callbackcss();
+		}
+	}
+	getFiles(function(){
+		var f;
+		setTimeout(function(){
+			f= folder+'out.js';
+			compressor.minify({
+			  compressor: 'no-compress',
+			  input: files,
+			  output: f,
+			  callback: function (err, min) {
+			  }
+			});
+			// Using UglifyJS 
+
+		    compressor.minify({
+		      compressor: 'uglifyjs',
+		      input: f,
+		      output: folder+'new.min.js',
+		      callback: function (err, min) {
+		      	//res.download(path.join(__dirname, '../', folder,'new.min.js')); 
+		      	res.
+		      }
+		    });
+
+		},4000);
+	}, function(){
+		var f;
+		setTimeout(function(){
+			f= folder+'out.css';
+			compressor.minify({
+			  compressor: 'no-compress',
+			  input: files,
+			  output: f,
+			  callback: function (err, min) {
+			  }
+			});
+			// Using UglifyJS 
+
+			compressor.minify({
+			  compressor: 'sqwish',
+			  input: f,
+			  output: folder+'new.min.css',
+			  options: {
+			    strict: true 
+			  },
+			  callback: function (err, min) {
+		      	// var path = path.resolve(".")+folder+'new.min.css';
+		      	// res.download(path); // magic of download fuction
+		      	//res.sendFile(folder+'new.min.js' , { root : rootFolder+'/../'});
+		      	res.send("here");
+		        // res.status(200).json({success : true, message : 'success', new_link: 'http://'+ip.address()+'/'+folder+'new.min.js'}).sendFile(folder+'new.min.js' , { root : __dirname});
+		      	//res.download(folder+'new.min.js');
+		      }
+			});
+		},10000);
+	});
+	
+    
+});
+
+module.exports = router;
